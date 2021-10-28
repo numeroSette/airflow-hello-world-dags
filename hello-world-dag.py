625837c2ebe319c945848e6652df07fca380f009
Guilherme Vito Giuseppe Sette <developer.guilherme@gmail.com>
2021-10-28 16:25:02 -0300

    Update hello-world-dag.py

--- hello-world-dag.py
+++ hello-world-dag.py
@@ -1,21 +1,27 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
-from airflow.operators import MultiplyBy5Operator
+import sys
 
 def print_hello():
- return 'Hello Wolrd'
+    sys.stdout = open("test.txt", "w")
 
-dag = DAG('hello_world', description='Hello world example', schedule_interval='0 12 * * *', start_date=datetime(2017, 3, 20), catchup=False)
+    print("Hello World")
 
-dummy_operator = DummyOperator(task_id='dummy_task', retries = 3, dag=dag)
+    sys.stdout.close()
+    return 'Hello world!'
+
+args = {
+    'owner': 'Lucas Baiao & Sette',
+    'depends_on_past': False,
+    'start_date': datetime(2021, 3, 22, 15, 0),
+    'retries': 3,
+    'retry_delay': timedelta(minutes=1)
+}
+
+dag = DAG(dag_id="hello_world", default_args=args, schedule_interval='5 * * * *', catchup=False, concurrency=1, max_active_runs=1)
+
+dummy_operator = DummyOperator(task_id='dummy_task', retries=3, dag=dag)
 
 hello_operator = PythonOperator(task_id='hello_task', python_callable=print_hello, dag=dag)
-
-multiplyby5_operator = MultiplyBy5Operator(my_operator_param='my_operator_param',
-                                task_id='multiplyby5_task', dag=dag)
-
-dummy_operator >> hello_operator
-
-dummy_operator >> multiplyby5_operator
